e616402e2caca1e6e104a5d2d5e9b3cd55b671fe
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2021-08-09 17:12:05 +0630

    Update kube.sh

--- Cluster-Setup/kube.sh
+++ Cluster-Setup/kube.sh
@@ -14,5 +14,8 @@ apt install docker-ce containerd.io -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 apt update && apt install -y kubeadm=1.18.5-00 kubelet=1.18.5-00 kubectl=1.18.5-00
+sed -i "s/cgroupDriver: systemd/cgroupDriver: cgroupfs/g" /var/lib/kubelet/config.yaml
+systemctl daemon-reload
+systemctl restart kubelet
 }
 
